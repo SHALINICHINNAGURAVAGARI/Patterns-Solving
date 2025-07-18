e1a458721d171b5c5c8f29f3bbbd6776ef014d6a
Shalu <shlokashalini@gmail.com>
2025-07-18 11:24:55 +0530

    Add pattern8

---
+++ Pattern8.java
@@ -0,0 +1,13 @@
+class Solution {
+  public void pattern8(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j < i; j++) {
+        System.out.print(" ");
+      }
+      for (int k = 1; k <= 2 * (n - i) + 1; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
